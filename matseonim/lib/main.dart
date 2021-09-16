ba2a43740ae7aff00e792e37e7cfedf6b66d8f8a
Jaedeok Kim <jdeokkim@protonmail.com>
2021-09-16 14:03:39 +0000

    카카오 네이티브 앱 키 추가

--- matseonim/lib/main.dart
+++ matseonim/lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:kakao_flutter_sdk/common.dart';
 
 import 'package:matseonim/theme.dart';
 import 'package:matseonim/pages/join_page.dart';
 
-void main() => runApp(MainApp());
+const String _kakaoNativeAppKey = "00e720cdadb5f87b4930ec8cc08179b4";
+
+void main() {
+  _initKakaoSDK();
+  runApp(MainApp());
+}
+
+void _initKakaoSDK() {
+  KakaoContext.clientId = _kakaoNativeAppKey;
+}
 
 class MainApp extends StatelessWidget {
   @override
