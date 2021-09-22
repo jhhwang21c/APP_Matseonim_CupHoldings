cdfc227560f400db9227fe008092294142d8b482
John Hwang <54915336+jhhwang21c@users.noreply.github.com>
2021-09-22 14:37:02 +0000

    `cloud_firestore` 플러그인 추가 및 Cloud Firestore 연동

--- matseonim/lib/main.dart
+++ matseonim/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:matseonim/pages/join_page1.dart';
+import 'package:matseonim/pages/main_page.dart';
 import 'package:matseonim/theme.dart';
 import 'package:matseonim/pages/login_page.dart';
 
@@ -15,7 +17,7 @@ class MainApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       theme: defaultTheme(),
-      home: LoginPage(),
+      home: JoinPage1(),
     );
   }
 }
