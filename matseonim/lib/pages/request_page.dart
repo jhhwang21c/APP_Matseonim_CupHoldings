feb539abb50295bb60ff34a2bed8b1248198413c
John Hwang <54915336+jhhwang21c@users.noreply.github.com>
2021-10-06 14:56:28 +0000

    의뢰 요청 페이지에 텍스트 필드 및 버튼 추가

--- matseonim/lib/pages/request_page.dart
+++ matseonim/lib/pages/request_page.dart
@@ -3,12 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:matseonim/components/autocomplete_form.dart';
 
 import 'package:matseonim/components/custom_app_bar.dart';
+import 'package:matseonim/components/custom_elevated_button.dart';
 import 'package:matseonim/components/custom_form_field.dart';
 import 'package:matseonim/components/multiline_textfield.dart';
+import 'package:matseonim/models/request.dart';
+import 'package:matseonim/models/user.dart';
 import 'package:matseonim/pages/drawer_page.dart';
 
 class RequestPage extends StatelessWidget {
-
   var titleTextController = TextEditingController();
   var descriptionTextController = TextEditingController();
   var interestTextController = TextEditingController();
@@ -18,26 +20,55 @@ class RequestPage extends StatelessWidget {
     return Scaffold(
       appBar: CustomAppBar(),
       drawer: DrawerPage(),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: ListView(
-          children: [
-            const Padding(
-              padding: EdgeInsets.symmetric(vertical: 20),
-              child: Text("의뢰하기", style: TextStyle(fontSize: 32)),
-            ),
-            ObscurableFormField(shouldObscure: false, textController: titleTextController, hintText: "제목"),
-            SizedBox(height: 20),
-            MultiLineTextField(textController: descriptionTextController),
-            SizedBox(height: 20),
-            AutocompleteForm(
-              hintText: "요청분야를 입력해주세요",
-              textController: interestTextController,
-            ),
-          ],
-        ),
+      body: FutureBuilder(
+        future: MSIUser.init(),
+        builder: (BuildContext context, AsyncSnapshot<MSIUser> snapshot) {
+          if (!snapshot.hasData) {
+            return SizedBox(
+                child: Container(
+                    alignment: Alignment.center,
+                    child: const CircularProgressIndicator()));
+          } else {
+            final MSIUser user = snapshot.data!;
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: ListView(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 20),
+                    child: Text("의뢰하기", style: TextStyle(fontSize: 32)),
+                  ),
+                  ObscurableFormField(
+                      shouldObscure: false,
+                      textController: titleTextController,
+                      hintText: "제목"),
+                  SizedBox(height: 20),
+                  MultiLineTextField(textController: descriptionTextController),
+                  SizedBox(height: 20),
+                  AutocompleteForm(
+                    hintText: "요청분야를 입력해주세요",
+                    textController: interestTextController,
+                  ),
+                  SizedBox(height: 20),
+                  CustomElevatedButton(
+                      text: "요청하기",
+                      textStyle:
+                          const TextStyle(fontSize: 16, color: Colors.white),
+                      color: Theme.of(context).primaryColor,
+                      funPageRoute: () {
+                        MSIRequests.add(
+                          uid: '${user.uid}',
+                          title: titleTextController.text,
+                          description: descriptionTextController.text,
+                          interest: interestTextController.text,
+                        );
+                      })
+                ],
+              ),
+            );
+          }
+        },
       ),
     );
   }
-
 }
